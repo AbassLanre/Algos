17f3863e23a77276007ef569cf96eb35c00b3106
Abass Lanre <kalanruse@gmail.com>
2022-01-10 09:46:18 +0100

    Implement strStr

---
+++ implement_strstr.py
@@ -0,0 +1,11 @@
+def strStr( haystack, needle):
+        if needle in haystack:
+            return haystack.index(needle)
+        
+        elif needle not in haystack:
+            return -1
+        # for index,value in enumerate(haystack):
+    
+    
+print(strStr('',''))
+            
