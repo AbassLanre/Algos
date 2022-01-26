cb4224c6220017ac9bfe79daaa7f7066cf6175c3
Abass Lanre <kalanruse@gmail.com>
2022-01-26 22:48:08 +0100

    initial commit

---
+++ Arrays/how_many_numbers_are_smaller.py
@@ -0,0 +1,12 @@
+def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+    output= []
+    count = 0
+
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if j != i and nums[j]< nums[i]:
+                count += 1
+        output.append(count)
+        count = 0
+
+    return output
