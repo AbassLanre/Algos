22f6ea7444024980af92dd7865a18a91c596645d
timilehin <oluwatimilehin.abass@lampnets.com>
2022-06-06 12:05:17 +0100

    initial commit

--- Arrays/remove_duplicate.py
+++ Arrays/remove_duplicate.py
@@ -1,14 +1,22 @@
 def removeDuplicates(nums):
-    new_nums=[]
-    count =1
-    for i in range(len(nums)-1):
-        if nums[i] == nums[i+1]:
-            count+=1
-        else:
-            new_nums.append(nums[i])
-    new_nums.append(nums[-1])
-    for i in range(count-1):
-        new_nums.append(0)
-    return new_nums
+    # new_nums=[]
+    # count =1
+    # for i in range(len(nums)-1):
+    #     if nums[i] == nums[i+1]:
+    #         count+=1
+    #     else:
+    #         new_nums.append(nums[i])
+    # new_nums.append(nums[-1])
+    # for i in range(count-1):
+    #     new_nums.append(0)
+    count = 0
 
-print(removeDuplicates([1,1,1,2,2,3,3,3,3]))
+    for i in nums:
+        while nums.count(i) > 1:
+            nums.pop(nums.index(i))
+            count = count + nums.count(i)
+    for g in range(count-1):
+        nums.append('_')
+    return nums,len(nums)
+
+print(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
